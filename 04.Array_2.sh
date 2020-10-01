510f9ac94fc23b55478975a27845bec4e886cee8
丁星元 <dingxingyuan@dingxingyuandeMacBook-Pro.local>
2020-10-01 10:04:58 +0800

    add script 04.Array_2.sh which is practise about Associative arrays.

---
+++ 04.Array_2.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/bash
+##############################
+#Author:        Ray Ding
+#Created Time:  2020-10-01_08:59:08
+#Version:       1.0
+#Description:   
+#    Associative arrays
+#    key-value array ( I treat it as dict in python)
+##############################
+# declare a new array (dict)
+declare -A COLOR=(["red"]="#ff0000"  ["green"]="#00ff00")
+
+echo ${COLOR[@]}
+echo ${COLOR[*]}
+
+# add a new key-value pair
+
+COLOR["blue"]="#0000ff"
+
+# get all values
+echo "get all values:"
+
+for value in ${COLOR[@]}
+do
+    echo $value
+done
+
+# get all keys
+echo "get all keys:"
+
+for key in ${!COLOR[@]}
+do
+    echo $key
+done
+
+# get all key - values
+echo "get all key - values:"
+
+for key in ${!COLOR[@]}
+do
+    echo "$key ---> ${COLOR[$key]}"
+done
+# 
